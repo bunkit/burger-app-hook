251799edfa29691fd2033e31d01531c2c752b280
bangkit <bangkitawe@gmail.com>
2020-12-18 14:10:32 +0700

    test contibute

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux/Aux';
-import classes from './Modal.module.css'
-import BackDrop from '../Backdrop/Backdrop';
+import React from "react";
+import Aux from "../../../hoc/Aux/Aux";
+import classes from "./Modal.module.css";
+import BackDrop from "../Backdrop/Backdrop";
 
 const Modal = (props) => {
     return (
@@ -10,19 +10,21 @@ const Modal = (props) => {
             <div
                 className={classes.Modal}
                 style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    transform: props.show
+                        ? "translateY(0)"
+                        : "translateY(-100vh)",
                     opacity: props.show ? 1 : 0,
                 }}
             >
                 {props.children}
             </div>
         </Aux>
-    )
-}
+    );
+};
 
 export default React.memo(Modal, (prevProps, nextProps) => {
     return (
-        prevProps.show === nextProps.show || prevProps.children === nextProps.children
-        // false
-    )
+        prevProps.show === nextProps.show &&
+        prevProps.children === nextProps.children
+    );
 });
